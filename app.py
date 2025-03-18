ec18c33a0b7596cceb8b2fa0c4a72fa3559a98c4
brij9557 <brijmohan.sharma9557@gmail.com>
2025-03-18 22:24:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,5 @@ def merge_pdf():
     return send_file(merged_pdf_path, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    
